ce4dae3b5963fb206f79f4e71c11e67a539e5b65
justiniantheemperor <justinrellis52@gmail.com>
2023-09-10 16:30:35 -0600

    optimize DropDown code

--- Family Drop-Down/ClientApp/src/components/DropDown.js
+++ Family Drop-Down/ClientApp/src/components/DropDown.js
@@ -2,69 +2,52 @@
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 
-const DropDown = () => {
+const formatDisplay = (person) => {
+    const fullName = `${person.givenName} ${person.surname}`;
+    let dateRange = "";
 
+    if (person.birthDate && person.deathDate) {
+        dateRange = `(${person.birthDate.split('-')[0]}-${person.deathDate.split('-')[0]})`;
+    } else if (person.deathDate) {
+        dateRange = `(-${person.deathDate.split('-')[0]})`;
+    } else if (person.birthDate) {
+        const birthYear = person.birthDate.split('-')[0];
+        const currentYear = new Date().getFullYear();
+        dateRange = currentYear - birthYear < 120 ? "(Living)" : `(${birthYear}-Living)`;
+    } else {
+        dateRange = "(Living)";
+    }
+
+    return `${fullName} ${dateRange}`;
+};
+
+const fetchPeopleData = async (familyTreeId) => {
+    try {
+        const response = await fetch(`person/${familyTreeId}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching data: ", error);
+        return [];
+    }
+};
+
+const DropDown = () => {
     const [people, setPeople] = useState([]);
     const familyTreeId = 1;
 
     useEffect(() => {
-        fetch(`person/${familyTreeId}`)
-            .then((results) => {
-                return results.json();
-            })
-            .then(data => {
-                setPeople(data);
-            })
-            .catch(error => {
-                console.error("Error fetching data: ", error);
-            });
-    }, [])
-
-    const formatData = (person) => {
-        if (person.birthDate && person.deathDate) {
-            // Person has both birth and death dates
-            return {
-                value: person.id,
-                display: `${person.givenName} (${person.birthDate.split('-')[0]}-${person.deathDate.split('-')[0]})`,
-            };
-        } else if (person.deathDate) {
-            // Person has only a death date
-            return {
-                value: person.id,
-                display: `${person.givenName} (-${person.deathDate.split('-')[0]})`,
-            };
-        } else if (person.birthDate) {
-            // Person has only a birth date
-            const birthYear = person.birthDate.split('-')[0];
-            const currentYear = new Date().getFullYear();
-            if (currentYear - birthYear < 120) {
-                // Person is less than 120 years old
-                return {
-                    value: person.id,
-                    display: `${person.givenName} (Living)`,
-                };
-            } else {
-                return {
-                    value: person.id,
-                    display: `${person.givenName} (${birthYear}-Living)`,
-                };
-            }
-        } else {
-            // Person has neither birth nor death date
-            return {
-                value: person.id,
-                display: `${person.givenName} (Living)`,
-            };
-        }
-    };
-
+        fetchPeopleData(familyTreeId).then((data) => {
+            setPeople(data);
+        });
+    }, [familyTreeId]);
 
     return (
         <main>
             <Autocomplete
                 id="person-select"
                 options={people}
-                getOptionLabel={(person) => formatData(person).display}
+                getOptionLabel={(person) => formatDisplay(person)}
                 renderInput={(params) => <TextField {...params} label="Select a person" />}
             />
         </main>
